56d8563ccde41bb67b6742c214d1c4677088b84b
unknown <jonas.gastro91@gmail.com>
2023-05-24 18:32:58 -0300

    feat: login requiring status correctly

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import { Login } from '../pages/Login'
 
 function App() {
 
   return (
     <>
-   HELLO WORLD
+      <Login />
     </>
   )
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-export function Login () {
+export function Login() {
+  const [apiKey, setApiKey] = useState('');
+  const [isValidKey, setIsValidKey] = useState(true);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.get('https://v3.football.api-sports.io/status', {
+        headers: {
+          'x-apisports-key': apiKey
+        }
+      });
+      
+      console.log(response.data.response);
+      console.log(response.data.response.subscription.active);
+      const { active } = response.data.response.subscription;
+
+      if (response.data.response) {
+        alert("Pode iniciar escolhendo um país")
+      } else {
+        setIsValidKey(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h2 className="">Welcome to My Team</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="userKey"
+          placeholder="Chave de entrada"
+          value={apiKey}
+          onChange={(e) => setApiKey(e.target.value)}
+        />
+        {!isValidKey && <p>Chave inválida</p>}
+
+        <button type="submit">Entrar</button>
+
+      </form>
+    </div>
+  );
 }
